c564c10197b99021330eb4fd3692868f0d80e247
viniciusalves226 <viniciusalves@eaportal.org>
2025-02-10 14:55:04 -0300

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,21 @@
+
+#include <stdio.h> //biblioteca de entrada e saida (scanf prntf etc...)
+
+int main()
+{
+	int opcao = 0;
+	int i;
+	
+	while (opcao != 5) {
+	    printf("----MENU----");
+	    printf("1 - BEBIDAS");
+	    printf("2 - MASSAS");
+	    printf("3 - FRIOS");
+	    printf("4 - PORÇOES");
+	    printf("5 - SAIR");
+	    scanf("%d, &");
+	    
+	}
+	
+	return 0;
+}
