dcfcd27d9f72b3eaff6e333ffc827e01e42f0573
viniciusalves226 <viniciusalves@eaportal.org>
2025-02-17 14:48:42 -0300

    Add files via upload

--- main (2).c
+++ main (2).c
@@ -1,21 +1,32 @@
+#include <stdio.h>
 
-#include <stdio.h> //biblioteca de entrada e saida (scanf prntf etc...)
+int soma(int numero1, int numero2) {
+    return numero1 + numero2;
+}
+int mutiplicar(int numero1, int numero2) {
+    return numero1 * numero2;
+}
 
-int main()
-{
-	int opcao = 0;
-	int i;
-	
-	while (opcao != 5) {
-	    printf("----MENU----");
-	    printf("1 - BEBIDAS");
-	    printf("2 - MASSAS");
-	    printf("3 - FRIOS");
-	    printf("4 - PORÇOES");
-	    printf("5 - SAIR");
-	    scanf("%d, &");
-	    
-	}
-	
-	return 0;
+ float calcularmedia(float numero1, float numero2) {
+    return (numero1 + numero2)/2;
+}
+
+void imprimirResultadoMedia() {  
+     float numero1, numero2, resultado;
+        
+        printf("Digite o primeiro numero : ");
+        scanf("%f", &numero1);
+        
+        printf("Digite o primeiro numero : ");
+        scanf("%f",&numero2);
+        
+        resultado = calcularmedia(numero1, numero2);
+        
+        printf("\nA media do numero %.1f + %.1f eh igual: %.1f", numero1, numero2, resultado);
+}
+
+int main() {
+   imprimirResultadoMedia();
+        
+    return 0;
 }
